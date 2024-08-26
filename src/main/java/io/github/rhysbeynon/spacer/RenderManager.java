5fca077e2dada320746c98494699609204721f6e
NewtGQ <rhysb2102@gmail.com>
2024-08-26 04:47:53 +0100

    shader stuff done, Current validation problem persists.

--- src/main/java/io/github/rhysbeynon/spacer/RenderManager.java
+++ src/main/java/io/github/rhysbeynon/spacer/RenderManager.java
@@ -2,6 +2,7 @@ package io.github.rhysbeynon.spacer;
 
 import io.github.rhysbeynon.spacer.test.Launcher;
 import io.github.rhysbeynon.spacer.entity.Model;
+import io.github.rhysbeynon.spacer.utilities.Utilities;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
@@ -9,23 +10,31 @@ import org.lwjgl.opengl.GL30;
 public class RenderManager {
 
     private final WindowManager window;
+    private  ShaderManager shader;
 
     public RenderManager() {
         window = Launcher.getWindow();
     }
 
     public void init() throws Exception {
+        shader = new ShaderManager();
 
+        shader.createVertexShader(Utilities.loadResource("/shaders/vertex.vs"));
+
+        shader.createFragmentShader(Utilities.loadResource("/shaders/fragment.fs"));
+
+        shader.link();
     }
 
     public void render(Model model) {
         clear();
+        shader.bind();
         GL30.glBindVertexArray(model.getId());
         GL30.glEnableVertexAttribArray(0);
         GL30.glDrawArrays(GL11.GL_TRIANGLES, 0, model.getVertexCount());
         GL30.glDisableVertexAttribArray(0);
         GL30.glBindVertexArray(0);
-
+        shader.unbind();
     }
 
     public void clear() {
@@ -33,6 +42,6 @@ public class RenderManager {
     }
 
     public void cleanup() {
-
+        shader.cleanup();
     }
 }
--- src/main/java/io/github/rhysbeynon/spacer/utilities/Utilities.java
+++ src/main/java/io/github/rhysbeynon/spacer/utilities/Utilities.java
@@ -2,7 +2,12 @@ package io.github.rhysbeynon.spacer.utilities;
 
 import org.lwjgl.system.MemoryUtil;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class Utilities {
 
@@ -10,6 +15,21 @@ public class Utilities {
         FloatBuffer buffer = MemoryUtil.memAllocFloat(data.length);
         buffer.put(data).flip();
         return buffer;
+    }
+
+    public static IntBuffer storeDataInIntBuffer(int[] data) {
+        IntBuffer buffer = MemoryUtil.memAllocInt(data.length);
+        buffer.put(data).flip();
+        return buffer;
+    }
+
+    public static String loadResource(String filename) throws Exception {
+        String result;
+        try(InputStream in = Utilities.class.getResourceAsStream(filename);
+            Scanner scanner = new Scanner(in, StandardCharsets.UTF_8.name())) {
+            result = scanner.useDelimiter("\\A").next();
+        }
+        return result;
     }
 
 }
